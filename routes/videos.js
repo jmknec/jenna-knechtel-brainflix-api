e95e4fe83e9b28ebbfde79f05f3575502c327f8f
Jenna Knechtel <jmknec@gmail.com>
2024-06-10 18:11:37 -0400

    create post method & re-test videos route

--- routes/videos.js
+++ routes/videos.js
@@ -1,5 +1,6 @@
 import express from "express";
 import fs from "fs";
+import { v4 } from "uuid";
 
 const videosRouter = express.Router();
 
@@ -8,9 +9,26 @@ const readVideos = () => {
   return JSON.parse(videoData);
 };
 
-videosRouter.route("/").get((_req, res) => {
-  const videosData = readVideos();
-  res.json(videosData);
-});
+videosRouter
+  .route("/")
+  .get((_req, res) => {
+    const videosData = readVideos();
+    res.json(videosData);
+  })
+  .post((req, res) => {
+    const { title, description } = req.body;
+
+    const videoData = readVideos();
+    const newVideo = {
+      id: v4(),
+      title,
+      description,
+    };
+    videoData.push(newVideo);
+
+    fs.writeFileSync("./data/videos.json", JSON.stringify(videoData));
+
+    res.send("Thank you for uploading a video");
+  });
 
 export default videosRouter;
